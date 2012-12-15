1907e22ad9ab9d8bfcdb572b52e38db4aeb7d441
Si Yu <siyu798@gmail.com>
2012-12-15 01:42:42 -0500

    init

---
+++ init.el
@@ -0,0 +1,56 @@
+;; https://github.com/technomancy/swank-clojure
+;; 
+(setenv "PATH" (shell-command-to-string "source $HOME/.bashrc && printf $PATH"))
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+;(set-face-attribute 'default nil :height 130)
+;(load-theme 'zenburn t)
+
+(set-face-attribute 'default nil :font "Consolas-11")
+
+(load-file (concat (file-name-as-directory (concat user-emacs-directory "non-elpa")) "cyberpunk.el"))
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "#333333")
+(color-theme-cyberpunk)
+(set-cursor-color "yellow")
+
+;; undo-tree
+(require 'undo-tree)
+(global-undo-tree-mode)
+
+;; find-file-in-project
+(global-set-key (kbd "C-x f") 'find-file-in-project)
+
+;; nrepl
+(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+
+;; Stop the error buffer from popping up while working in the REPL buffer
+(setq nrepl-popup-stacktraces nil)
+
+;; Make C-c C-z switch to the *nrepl* buffer in the current window
+(add-to-list 'same-window-buffer-names "*nrepl*")
+
+;;;;;;;;;;;;;;;;;;
+;; key bindings ;;
+;;;;;;;;;;;;;;;;;;
+
+;; winner undo and redo
+(global-set-key (kbd "C-c b") 'winner-undo)
+;(define-key nrepl-interaction-mode-map (kbd "C-c b") 'winner-undo)
+(global-set-key (kbd "C-c f") 'winner-redo)
+
+;; switch buffer window
+(global-set-key [C-tab] 'other-window)
+
+;;use delete-horizontal-space to completely nuke all whitespace
+(global-set-key (kbd "M-SPC ")   'live-delete-whitespace-except-one)
+
+;;kill regions
+(global-set-key (kbd "C-x C-k") 'kill-region)
+
+;; Ace jump mode
+(global-set-key (kbd "C-o") 'ace-jump-mode)
---
+++ siyu.el
@@ -0,0 +1,5 @@
+
+(menu-bar-mode 1)
+
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'control)
