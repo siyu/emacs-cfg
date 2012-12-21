b73c9b2edda6dc5fa887f06ef60129a2e44f4fad
Si Yu <siyu798@gmail.com>
2012-12-21 00:30:45 -0500

    added os specific settings

--- init.el
+++ init.el
@@ -1,10 +1,6 @@
-;; https://github.com/technomancy/swank-clojure
-;; 
-(setenv "PATH" (shell-command-to-string "source $HOME/.bashrc && printf $PATH"))
-
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
 ;(set-face-attribute 'default nil :height 130)
@@ -12,7 +8,9 @@
 
 (set-face-attribute 'default nil :font "Consolas-13")
 
-(load-file (concat (file-name-as-directory (concat user-emacs-directory "non-elpa")) "cyberpunk.el"))
+(setq non-elpa-dir (file-name-as-directory (concat user-emacs-directory "non-elpa")))
+
+(load-file (concat non-elpa-dir "cyberpunk.el"))
 (global-hl-line-mode 1)
 (set-face-background 'hl-line "#333333")
 (color-theme-cyberpunk)
@@ -70,3 +68,41 @@
 
 ;; Ace jump mode
 (global-set-key (kbd "C-o") 'ace-jump-mode)
+
+;; Mac
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'control)
+
+
+;;;; For OS specific settings
+(if (eq system-type 'windows-nt)  
+    (progn ;; Windows
+      ;; font style and size
+      (set-face-attribute 'default nil :font "Consolas-10.5")
+
+      ;; blackbored color theme
+      (load-file (concat non-elpa "blackbored.el"))
+      (color-theme-blackbored)
+      
+      ;; cygwin
+      (custom-set-variables
+       ;; custom-set-variables was added by Custom.
+       ;; If you edit it by hand, you could mess it up, so be careful.
+       ;; Your init file should contain only one such instance.
+       ;; If there is more than one, they won't work right.
+       '(cygwin-mount-cygwin-bin-directory "C:\\cygwin\\bin"))
+      (setq exec-path (cons "C:/cygwin/bin" exec-path))
+      (setenv "PATH" (concat "C:/cygwin/bin;" (getenv "PATH")))
+      (load-file (concat non-elpa "setup-cygwin.el"))
+      (require 'setup-cygwin)
+
+      ;; dos shell
+      (defun dos-shell ()
+        "Run dos in shell mode."
+        (interactive)
+        (let ((explicit-shell-file-name "C:/windows/system32/cmd"))
+          (call-interactively 'shell))))
+  (progn ;; *nix & mac
+    (setenv "PATH" (shell-command-to-string "source $HOME/.bashrc && printf $PATH"))))
+
+
--- siyu.el
+++
@@ -1,5 +0,0 @@
-
-(menu-bar-mode 1)
-
-(setq mac-command-modifier 'meta)
-(setq mac-option-modifier 'control)
