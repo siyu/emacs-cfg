47f5058e2bd25a6f266597222cae24cd53791b1c
Si Yu <siyu798@gmail.com>
2012-12-21 01:06:39 -0500

    auto-complete

--- init.el
+++ init.el
@@ -27,11 +27,45 @@
 (global-set-key (kbd "C-x f") 'find-file-in-project)
 
 ;; auto-complete
+(require 'auto-complete)
+(require 'auto-complete-config)
+(ac-config-default)
+(ac-flyspell-workaround)
+;;(add-to-list 'ac-dictionary-directories (concat (live-pack-lib-dir) "auto-complete/dict"))
+
+(global-auto-complete-mode t)
+(setq ac-auto-show-menu t)
+(setq ac-dwim t)
+(setq ac-use-menu-map t)
+(setq ac-quick-help-delay 1)
+(setq ac-quick-help-height 60)
+(setq ac-disable-inline t)
+(setq ac-show-menu-immediately-on-auto-complete t)
+(setq ac-auto-start 2)
+(setq ac-candidate-menu-min 0)
+
+(set-default 'ac-sources
+             '(ac-source-dictionary
+               ac-source-words-in-buffer
+               ac-source-words-in-same-mode-buffers
+               ac-source-semantic
+               ac-source-yasnippet))
+
+(dolist (mode '(magit-log-edit-mode log-edit-mode org-mode text-mode haml-mode
+                sass-mode yaml-mode csv-mode espresso-mode haskell-mode
+                html-mode nxml-mode sh-mode smarty-mode clojure-mode
+                lisp-mode textile-mode markdown-mode tuareg-mode))
+  (add-to-list 'ac-modes mode))
+
+(define-key ac-completing-map (kbd "C-M-n") 'ac-next)
+(define-key ac-completing-map (kbd "C-M-p") 'ac-previous)
+
 (require 'ac-nrepl)
 (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
 (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'nrepl-mode))
+
 
 ;; rainbow-delimiter
 (require 'rainbow-delimiters)
@@ -40,13 +74,26 @@
   (add-hook (intern (concat (symbol-name x) "-mode-hook")) 'rainbow-delimiters-mode))
 
 ;; nrepl
-(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+(add-hook 'nrepl-interaction-mode-hook
+          (lambda ()
+            (nrepl-turn-on-eldoc-mode)
+            (enable-paredit-mode)))
+
+(add-hook 'nrepl-mode-hook
+          (lambda ()
+            (nrepl-turn-on-eldoc-mode)
+            (enable-paredit-mode)))
+
+(add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
 
 ;; Stop the error buffer from popping up while working in the REPL buffer
 (setq nrepl-popup-stacktraces nil)
 
 ;; Make C-c C-z switch to the *nrepl* buffer in the current window
 (add-to-list 'same-window-buffer-names "*nrepl*")
+
+
+
 
 ;;;;;;;;;;;;;;;;;;
 ;; key bindings ;;
